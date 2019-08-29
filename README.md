90f425c3a0388bd9a5e77da098dce060b0cdbe3a
COE-18 <coe-18@Expert-iMac.local>
2019-08-29 16:02:05 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# PUCOLLEGE_PickerViewExample
---
+++ multiplecomponentInPickerview/LocationClass.swift
@@ -0,0 +1,13 @@
+//
+//  LocationClass.swift
+//  multiplecomponentInPickerview
+//
+//  Created by COE-18 on 29/08/19.
+//  Copyright © 2019 COE-18. All rights reserved.
+//
+
+import UIKit
+
+class LocationClass: NSObject {
+
+}
